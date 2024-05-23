aca4d44cd6418886b815311057dbadaeedfe72c7
Rona May Balangat <125886471+ronamay-0408@users.noreply.github.com>
2024-05-23 08:28:43 +0800

    Add files via upload

---
+++ employee_db.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 23, 2024 at 02:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `employee_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `pass` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `dept_no` int(11) NOT NULL,
+  `sal_no` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`userid`, `username`, `pass`, `email`, `dept_no`, `sal_no`) VALUES
+(1, 'ronamay', 'rona2003', 'ronamay@gmail.com', 1, 1),
+(2, 'Wooseok', 'wooseok32', 'byeonwooseok@gmail.com', 5, 5),
+(3, 'Rona May', 'rona022003', 'ronamaybalangat@gmail.com', 3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `dnumber` int(11) NOT NULL,
+  `dname` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`dnumber`, `dname`) VALUES
+(1, 'TechSupport'),
+(3, 'Research'),
+(5, 'Payroll');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deptsal`
+--
+
+CREATE TABLE `deptsal` (
+  `dnumber` int(11) NOT NULL DEFAULT 0,
+  `totalsalary` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `deptsal`
+--
+
+INSERT INTO `deptsal` (`dnumber`, `totalsalary`) VALUES
+(1, 104000),
+(3, 228800),
+(5, 185900);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`userid`),
+  ADD KEY `dept_no` (`dept_no`),
+  ADD KEY `sal_no` (`sal_no`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`dnumber`);
+
+--
+-- Indexes for table `deptsal`
+--
+ALTER TABLE `deptsal`
+  ADD PRIMARY KEY (`dnumber`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `dnumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`dept_no`) REFERENCES `department` (`dnumber`),
+  ADD CONSTRAINT `accounts_ibfk_2` FOREIGN KEY (`sal_no`) REFERENCES `deptsal` (`dnumber`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
